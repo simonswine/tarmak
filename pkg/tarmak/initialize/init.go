916d1a915d106580c28a0048c4ad4bd19e329b6b
Joshua  Van Leeuwen <joshua.vanleeuwen@jetstack.io>
2017-09-14 18:31:40 +0100

    Refactor questions

--- pkg/tarmak/initialize/init.go
+++ pkg/tarmak/initialize/init.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -73,150 +74,27 @@ func (i *Init) Run() (err error) {
 	}
 	*/
 
-	var environment, context, combinedName string
-	open := &utils.Open{
-		Query:    "What should be the name of the cluster?\nThe name consists of two parts seperated by a dash. First part is the environment name, second part the cluster name. Both names should be matching [a-z0-9]+",
-		Prompt:   "> ",
-		Required: true,
+	combinedName, environment, context := AskCombindName(conf)
+	cloudProvider, credentialsSource, credentials := AskProvider()
+	//awsRegion := AskRegion()
+	contact, err := AskEmail(conf)
+	if err != nil {
+		fmt.Print(err)
+		return nil
 	}
-	for environment == "" {
-		combinedName = open.Ask()
-		environment, context, err = parseContextName(combinedName)
-		if err != nil {
-			fmt.Print(err)
-			open.Query = ""
-		} else if err := conf.UniqueEnvironmentName(environment); err != nil {
-			fmt.Printf("Invalid environment name: %v", err)
-			environment = ""
-		}
-	}
-	// TODO ensure max length of both is not longer than 24 chars (verify that limit from AWS)
+	bucketPrefix := AskBucketPrefix(cloudProvider, conf)
+	publicZone := AskPublicZone(cloudProvider, conf)
+	privateZone := AskPrivateZone()
+	projectName := AskProjectName()
 
-	sel := &utils.Select{
-		Query:   "Select a provider",
-		Prompt:  "> ",
-		Choice:  &[]string{"AWS"},
-		Default: 1,
-	}
-	provider := sel.Ask()
-
-	sel = &utils.Select{
-		Query:   "Where should the credentials come from?",
-		Prompt:  "> ",
-		Choice:  &[]string{"AWS folder", "Vault Path"},
-		Default: 1,
-	}
-	credentialsSource := sel.Ask()
-
-	var profileName string
-	var vaultPrefix string
-	if credentialsSource == "AWS folder" {
-		open := &utils.Open{
-			Query:    "What is the profile name?",
-			Prompt:   "> ",
-			Required: true,
-		}
-		profileName = open.Ask()
-
-	} else {
-		open := &utils.Open{
-			Query:   "Which path should be used for AWS credentials?",
-			Prompt:  "> ",
-			Default: "jetstack/aws/jetstack-dev/sts/admin",
-		}
-		vaultPrefix = open.Ask()
-	}
-
-	open = &utils.Open{
-		Query:    "What is the mail address of someone responsible?",
-		Prompt:   "> ",
-		Default:  conf.Contact(),
-		Required: true,
-	}
-	contact := open.Ask()
-
-	var fail error
-	if err = utils.ValidateFormat(contact); err != nil {
-		fail = err
-
-		// Not sure if this is a good idea bc of privacy concerns
-	} else if err = utils.ValidateHost(contact); err != nil {
-		fail = err
-	}
-	if fail != nil {
-		yn := &utils.YesNo{
-			Query:   fmt.Sprintf("Error verifying email, did you spell it correctly?:\n%v\nUse anyway?", fail),
-			Prompt:  "> ",
-			Default: true,
-		}
-		if !yn.Ask() {
-			fmt.Printf("Aborting.\n")
-			return nil
-		}
-	}
-
-	sel = &utils.Select{
-		Query:  "Which region should be used for DNS records?",
-		Prompt: "> ",
-		Choice: &[]string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "ca-central-1", "eu-west-1", "eu-central-1", "eu-west-2", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-south-1", "sa-east-1", "enter custom zone"},
-	}
-	awsRegion := sel.Ask()
-
-	if awsRegion == "enter custom zone" {
-		open := &utils.Open{
-			Query:    "Enter custom zone",
-			Prompt:   "> ",
-			Required: true,
-		}
-		awsRegion = open.Ask()
-	}
-	// TODO: validate region
-
-	var bucketPrefix string
-	open = &utils.Open{
-		Query:   "What is the bucket prefix?",
-		Prompt:  "> ",
-		Default: "tarmak-",
-	}
-	for bucketPrefix == "" {
-		bucketPrefix = open.Ask()
-		if err := conf.ValidName(bucketPrefix, "[a-z0-9-_]+"); err != nil {
-			fmt.Printf("Name is not valid: %v", err)
-			open.Query = ""
-			bucketPrefix = ""
-		}
-	}
-
-	open = &utils.Open{
-		Query:    "What public zone should be used?\nPlease make sure you can delegate this zone.",
-		Prompt:   "> ",
-		Required: true,
-	}
-	publicZone := open.Ask()
-	// TODO: verify domain name
-
-	open = &utils.Open{
-		Query:   "What private zone should be used?",
-		Prompt:  "> ",
-		Default: "tarmak.local",
-	}
-	privateZone := open.Ask()
-	// TODO: verify domain name
-
-	open = &utils.Open{
-		Query:   "What is the project name?",
-		Prompt:  "> ",
-		Default: "k8s-playground",
-	}
-	projectName := open.Ask()
-
-	fmt.Printf("\nEnvironment --->%s\n", environment)
+	fmt.Printf("\nCombined Name ->%s\n", combinedName)
+	fmt.Printf("Environment --->%s\n", environment)
 	fmt.Printf("Context ------->%s\n", context)
-	fmt.Printf("Cloud Provider >%s\n", provider)
+	fmt.Printf("Cloud Provider >%s\n", cloudProvider)
 	if credentialsSource == "AWS folder" {
-		fmt.Printf("Profile Name -->%s\n", profileName)
+		fmt.Printf("Profile Name -->%s\n", credentials)
 	} else {
-		fmt.Printf("Vault Prefix -->%s\n", vaultPrefix)
+		fmt.Printf("Vault Prefix -->%s\n", credentials)
 	}
 	fmt.Printf("Contact ------->%s\n", contact)
 	fmt.Printf("Bucket Prefix ->%s\n", bucketPrefix)
@@ -277,4 +155,225 @@ func (i *Init) Run() (err error) {
 
 	//return i.tarmak.MergeEnvironment(env)
 	return nil
+}
+
+func AskCombindName(conf interfaces.Config) (combinedName, environment, context string) {
+	var err error
+	open := &utils.Open{
+		Query:    "What should be the name of the cluster?\nThe name consists of two parts seperated by a dash. First part is the environment name, second part the cluster name. Both names should be matching [a-z0-9]+",
+		Prompt:   "> ",
+		Required: true,
+	}
+
+	for environment == "" {
+		combinedName = open.Ask()
+		environment, context, err = parseContextName(combinedName)
+		if err != nil {
+			fmt.Print(err)
+			open.Query = ""
+		} else if err := conf.UniqueEnvironmentName(environment); err != nil {
+			fmt.Printf("Invalid environment name: %v", err)
+			environment = ""
+		}
+	}
+	// TODO ensure max length of both is not longer than 24 chars (verify that limit from AWS)
+
+	return combinedName, environment, context
+}
+
+func AskProvider() (provider, credentialsSource, credentials string) {
+	sel := &utils.Select{
+		Query:   "Select a provider",
+		Prompt:  "> ",
+		Choice:  &[]string{"AWS"},
+		Default: 1,
+	}
+	provider = sel.Ask()
+
+	sel = &utils.Select{
+		Query:   "Where should the credentials come from?",
+		Prompt:  "> ",
+		Choice:  &[]string{"AWS folder", "Vault Path"},
+		Default: 1,
+	}
+	credentialsSource = sel.Ask()
+
+	if credentialsSource == "AWS folder" {
+		open := &utils.Open{
+			Query:    "What is the profile name?",
+			Prompt:   "> ",
+			Required: true,
+		}
+		credentials = open.Ask()
+
+	} else {
+		open := &utils.Open{
+			Query:   "Which path should be used for AWS credentials?",
+			Prompt:  "> ",
+			Default: "jetstack/aws/jetstack-dev/sts/admin",
+		}
+		credentials = open.Ask()
+	}
+
+	return provider, credentialsSource, credentials
+}
+
+//func AskRegion() (awsRegion string) {
+//
+//	sel := &utils.Select{
+//		Query:  "Which region should be used for DNS records?",
+//		Prompt: "> ",
+//		Choice: &[]string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "ca-central-1", "eu-west-1", "eu-central-1", "eu-west-2", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-south-1", "sa-east-1", "enter custom zone"},
+//	}
+//	awsRegion = sel.Ask()
+//
+//	if awsRegion == "enter custom zone" {
+//		open := &utils.Open{
+//			Query:    "Enter custom zone",
+//			Prompt:   "> ",
+//			Required: true,
+//		}
+//		awsRegion = open.Ask()
+//	}
+//	// TODO: validate region
+//
+//	return awsRegion
+//}
+
+func AskEmail(conf interfaces.Config) (contact string, err error) {
+	open := &utils.Open{
+		Query:    "What is the mail address of someone responsible?",
+		Prompt:   "> ",
+		Default:  conf.Contact(),
+		Required: true,
+	}
+	contact = open.Ask()
+
+	var fail error
+	if err = utils.ValidateFormat(contact); err != nil {
+		fail = err
+
+		// Not sure if this is a good idea bc of privacy concerns
+	} else if err = utils.ValidateHost(contact); err != nil {
+		fail = err
+	}
+	if fail != nil {
+		yn := &utils.YesNo{
+			Query:   fmt.Sprintf("Error verifying email, did you spell it correctly?:\n%v\nUse anyway?", fail),
+			Prompt:  "> ",
+			Default: true,
+		}
+		if !yn.Ask() {
+			return "", errors.New("Aborting")
+		}
+	}
+
+	return contact, nil
+}
+
+func AskBucketPrefix(cloudProvider string, conf interfaces.Config) (bucketPrefix string) {
+	query := "Whats is the s3 bucket prefix?"
+	if cloudProvider != "AWS" {
+		query = "What is the resource prefix?"
+	}
+
+	open := &utils.Open{
+		Query:   query,
+		Prompt:  "> ",
+		Default: "tarmak-",
+	}
+	for bucketPrefix == "" {
+		bucketPrefix = open.Ask()
+		if err := conf.ValidName(bucketPrefix, "[a-z0-9-_]+"); err != nil {
+			fmt.Printf("Name is not valid: %v", err)
+			open.Query = ""
+			bucketPrefix = ""
+		}
+	}
+
+	return bucketPrefix
+}
+
+func AskPublicZone(cloudProvider string, conf interfaces.Config) (publicZone string) {
+
+	// TODO: Validate if the zone is delegated in through the root servers
+
+	if cloudProvider == "AWS" {
+		zones := make(map[string]bool)
+		var choice []string
+
+		// create map of all zones used in all providers
+		for _, p := range conf.Providers() {
+			if p.AWS != nil {
+				zones[p.AWS.PublicZone] = true
+			}
+		}
+
+		// put list in []string for input select
+		for zone := range zones {
+			choice = append(choice, zone)
+		}
+		choice = append(choice, "enter custom zone")
+
+		sel := &utils.Select{
+			Query:   "Select public zone",
+			Prompt:  "> ",
+			Choice:  &choice,
+			Default: 1,
+		}
+		publicZone = sel.Ask()
+	}
+
+	if cloudProvider != "AWS" || publicZone == "enter custom zone" {
+		open := &utils.Open{
+			Query:    "What public zone should be used?\nPlease make sure you can delegate this zone.",
+			Prompt:   "> ",
+			Required: true,
+		}
+		publicZone = open.Ask()
+	}
+
+	return publicZone
+}
+
+func AskPrivateZone() (privateZone string) {
+
+	// TODO: verify domain name
+
+	open := &utils.Open{
+		Query:   "What private zone should be used?",
+		Prompt:  "> ",
+		Default: "tarmak.local",
+	}
+	return open.Ask()
+}
+
+func AskProjectName() (projectName string) {
+
+	open := &utils.Open{
+		Query:   "What is the project name?",
+		Prompt:  "> ",
+		Default: "k8s-playground",
+	}
+	return open.Ask()
+}
+
+func AskProviderName(conf interfaces.Config) (providerName string) {
+	open := &utils.Open{
+		Query:    "Enter a unique name for this provider",
+		Prompt:   "> ",
+		Required: true,
+	}
+
+	for providerName == "" {
+		resp := open.Ask()
+		if err := conf.ValidName(resp, "[a-z0-9-]+"); err != nil {
+			fmt.Printf("Name is not valid: %v", err)
+		} else if err := conf.UniqueProviderName(resp); err != nil {
+			fmt.Printf("Name is not valid: %v", err)
+		} else {
+			providerName = resp
+		}
+	}
+	return providerName
 }
--- pkg/tarmak/provider/init.go
+++ pkg/tarmak/provider/init.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/jetstack/tarmak/pkg/tarmak/config"
+	"github.com/jetstack/tarmak/pkg/tarmak/initialize"
 	"github.com/jetstack/tarmak/pkg/tarmak/interfaces"
 	"github.com/jetstack/tarmak/pkg/tarmak/utils"
 )
@@ -19,105 +20,10 @@ func Init(t interfaces.Tarmak) {
 		conf.InitConfig()
 	}
 
-	sel := &utils.Select{
-		Query:   "Select a provider",
-		Prompt:  "> ",
-		Choice:  &[]string{"AWS"},
-		Default: 1,
-	}
-	cloudProvider := sel.Ask()
-
-	var name string
-	open := &utils.Open{
-		Query:    "Enter a unique name for this provider",
-		Prompt:   "> ",
-		Required: true,
-	}
-	for name == "" {
-		resp := open.Ask()
-		if err := conf.ValidName(resp, "[a-z0-9-]+"); err != nil {
-			fmt.Printf("Name is not valid: %v", err)
-		} else if err := conf.UniqueProviderName(resp); err != nil {
-			fmt.Printf("Name is not valid: %v", err)
-		} else {
-			name = resp
-		}
-	}
-
-	sel = &utils.Select{
-		Query:   "Where should the credentials come from?",
-		Prompt:  "> ",
-		Choice:  &[]string{"AWS folder", "Vault"},
-		Default: 1,
-	}
-	credentialsSource := sel.Ask()
-
-	var profileName string
-	var vaultPrefix string
-	if credentialsSource == "AWS folder" {
-		open := &utils.Open{
-			Query:    "What is the profile name?",
-			Prompt:   "> ",
-			Required: true,
-		}
-		profileName = open.Ask()
-
-	} else {
-		open := &utils.Open{
-			Query:   "Which path should be used for AWS credentials?",
-			Prompt:  "> ",
-			Default: "jetstack/aws/jetstack-dev/sts/admin",
-		}
-		vaultPrefix = open.Ask()
-	}
-
-	query := "Whats is the s3 bucket prefix?"
-	if cloudProvider != "AWS" {
-		query = "What is the resource prefix?"
-	}
-	open = &utils.Open{
-		Query:    query,
-		Prompt:   "> ",
-		Required: true,
-	}
-	resourcePrefix := open.Ask()
-
-	// TODO: Validate if the zone is delegated in through the root servers
-	var publicZone string
-	if cloudProvider == "AWS" {
-		zones := make(map[string]bool)
-		var choice []string
-
-		// create map of all zones used in all providers
-		for _, p := range conf.Providers() {
-			if p.AWS != nil {
-				zones[p.AWS.PublicZone] = true
-			}
-		}
-
-		// put list in []string for input select
-		for zone := range zones {
-			choice = append(choice, zone)
-		}
-		choice = append(choice, "enter custom zone")
-
-		sel = &utils.Select{
-			Query:   "Select public zone",
-			Prompt:  "> ",
-			Choice:  &choice,
-			Default: 1,
-		}
-		publicZone = sel.Ask()
-	}
-
-	if cloudProvider != "AWS" || publicZone == "enter custom zone" {
-		open = &utils.Open{
-			Query:    "What public zone should be used?\nPlease make sure you can delegate this zone.",
-			Prompt:   "> ",
-			Required: true,
-		}
-		publicZone = open.Ask()
-	}
+	cloudProvider, credentialsSource, credentials := initialize.AskProvider()
+	providerName := initialize.AskProviderName(conf)
+	bucketPrefix := initialize.AskBucketPrefix(cloudProvider, conf)
+	publicZone := initialize.AskPublicZone(cloudProvider, conf)
 
 	/* This will be generated from the s3 bucket prefix right now. Not too sure but would like to keep it like that. Maybe we call the bucket_prefix resource_prefix for provider wide resources */
 	//open = &utils.Open{
@@ -128,15 +34,15 @@ func Init(t interfaces.Tarmak) {
 	//dynamoDbLockName := open.Ask()
 
 	fmt.Printf("\nCloud Provider ---->%s\n", cloudProvider)
-	fmt.Printf("Provider Name ----->%s\n", name)
+	fmt.Printf("Provider Name ----->%s\n", providerName)
 	fmt.Printf("Credentials Source >%s\n", credentialsSource)
 	if credentialsSource == "AWS folder" {
-		fmt.Printf("Profile Name ------>%s\n", profileName)
+		fmt.Printf("Profile Name ------>%s\n", credentials)
 	} else {
-		fmt.Printf("Vault Prefix ------>%s\n", vaultPrefix)
+		fmt.Printf("Vault Prefix ------>%s\n", credentials)
 	}
 	fmt.Printf("Public Zone ------->%s\n", publicZone)
-	fmt.Printf("Resource Prefix --->%s\n", resourcePrefix)
+	fmt.Printf("Resource Prefix --->%s\n", bucketPrefix)
 
 	yn := &utils.YesNo{
 		Query:   "Are these input correct?",
@@ -144,9 +50,9 @@ func Init(t interfaces.Tarmak) {
 		Default: true,
 	}
 	if yn.Ask() && cloudProvider == "AWS" {
-		prov := config.NewAWSProfileProvider(name, profileName)
+		prov := config.NewAWSProfileProvider(providerName, credentials)
 		prov.AWS.PublicZone = publicZone
-		prov.AWS.BucketPrefix = resourcePrefix
+		prov.AWS.BucketPrefix = bucketPrefix
 		conf.AppendProvider(prov)
 
 		fmt.Print("Accepted.\n")
